1e820b23517c35f329b14e1b0d7ee561431b5c54
sdCaramel302812 <kypss93097@gmail.com>
2021-12-27 00:41:53 -0500

    Home page outline

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import 'src/styles/Common.scss';
 
 function Header() {
   return (
-    <header className='App-header'>
+    <header className='App-Header'>
       <div className='Header-Container'>
         <img src={logo} className='App-logo' alt='logo' />
         <button className='Primary-Button Login-Button'>Login</button>
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import Header from 'src/components/Header';
+import 'src/styles/Common.scss';
+import 'src/styles/Home.scss';
 
 function UnLoggedIn() {
   return (
@@ -9,7 +11,15 @@ function UnLoggedIn() {
 
 function LoggedIn() {
   return (
-    <div>Hello</div>
+    <div className='Home'>
+      <div>
+        <span style={{verticalAlign: '-webkit-baseline-middle', fontSize: 20}}>My Projects</span>
+        <button className='Outline-Button New-Project-Button'>new</button>
+      </div>
+      <div>
+        <div className='Project-Container'>Hello</div>
+      </div>
+    </div>
   );
 }
 
